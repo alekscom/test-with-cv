a8fa5bd7a3f625cc684bfa66f710f2082cfcc528
alekscom <31254569+alekscom@users.noreply.github.com>
2018-09-13 09:37:42 +0200

    Add files via upload

---
+++ chart.js
@@ -0,0 +1,77 @@
+var javaData = [
+    {
+        value: 25,
+        color: "#ffd633"
+    },
+    {
+        value : 75,
+        color : "#E2E1E1"
+    },
+];
+var java= document.getElementById("java").getContext("2d");
+new Chart(java).Doughnut(javaData);
+
+var phpData = [
+    {
+        value: 45,
+        color: "#ffd633"
+    },
+    {
+        value : 55,
+        color : "#E2E1E1"
+    },
+];
+var php = document.getElementById("php").getContext("2d");
+new Chart(php).Doughnut(phpData);
+
+var cData = [
+    {
+        value: 50,
+        color: "#ffd633"
+    },
+    {
+        value : 50,
+        color : "#E2E1E1"
+    },
+];
+var c = document.getElementById("c").getContext("2d");
+new Chart(c).Doughnut(cData);
+
+var htmlData = [
+    {
+        value: 70,
+        color: "#ffd633"
+    },
+    {
+        value : 30,
+        color : "#E2E1E1"
+    },
+];
+var html = document.getElementById("html").getContext("2d");
+new Chart(html).Doughnut(htmlData);
+
+var cssData = [
+    {
+        value: 85,
+        color: "#ffd633"
+    },
+    {
+        value : 15,
+        color : "#E2E1E1"
+    },
+];
+var css = document.getElementById("css").getContext("2d");
+new Chart(css).Doughnut(cssData);
+
+var swiftData = [
+    {
+        value: 90,
+        color: "#ffd633"
+    },
+    {
+        value : 10,
+        color : "#E2E1E1"
+    },
+];
+var swift = document.getElementById("swift").getContext("2d");
+new Chart(swift).Doughnut(swiftData);
